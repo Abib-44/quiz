b31f2db800bb56cd69f94a35e7c8433fe6d52503
el <eh.benaboud.dev@gmail.com>
2026-02-28 22:40:04 +0100

    read db config from env

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://yourUser:changeit@postgres:5432/postgres"
+DATABASE_URL = (
+    f"postgresql+psycopg2://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PW']}"
+    f"@{os.environ['POSTGRES_HOST']}:{os.environ['POSTGRES_PORT']}/{os.environ['POSTGRES_DB']}"
+)
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
